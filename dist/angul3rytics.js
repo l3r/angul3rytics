96945cb2c9ca24f1607f752513c55f255f0ab4e5
l3r <mail@l3r.net>
2015-06-19 06:53:09 +0300

    Update angul3rytics.js
    back to easy

--- dist/angul3rytics.js
+++ dist/angul3rytics.js
@@ -48,7 +48,6 @@
                         }
                     });
                 };
-
                 service.trackTiming = function (category, variable, value, opt_label) {
                     forEachHandlerDo(function (handler) {
                         if (category && variable && value) {
@@ -56,34 +55,9 @@
                         }
                     });
                 };
-
                 $rootScope.$on(pageChangeEvent, function () {
                     service.trackPageView($location.url());
                 });
-
-                //BE added for ecommerce order tracking
-                service.trackEcommerceTrans = function (transactionId, affiliation, total, tax, shipping, city, state, country) {
-                    forEachHandlerDo(function (handler) {
-                        if (transactionId) {
-                            handler.trackEcommerceTrans(transactionId, affiliation, total, tax, shipping, city, state, country);
-                        }
-                    });
-                };
-                //BE added for ecommerce item tracking
-                service.trackEcommerceItem = function (transactionId, sku, name, category, price, quantity) {
-                    forEachHandlerDo(function (handler) {
-                        if (transactionId) {
-                            handler.trackEcommerceItem(transactionId, sku, name, category, price, quantity);
-                        }
-                    });
-                };
-                //BE added for ecommerce item tracking
-                service.pushTransaction = function () {
-                    forEachHandlerDo(function (handler) {
-                        handler.pushTransaction();
-                    });
-                };
-
                 return service;
             }
         ];
@@ -100,90 +74,48 @@
             service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
                 $log.log('Event tracked', category, action, opt_label, opt_value, opt_noninteraction);
             };
-
             service.trackTiming = function (category, variable, value, opt_label) {
                 $log.log('Timing tracked', category, variable, value, opt_label);
             };
-            //BE added for ecommerce order tracking
-            service.trackEcommerceTrans = function (transactionId, affiliation, total, tax, shipping, city, state, country) {
-                $log.log('TRANSACTION Tracked', transactionId, affiliation, total, tax, shipping, city, state, country);
-            };
-            //BE added for ecommerce item tracking
-            service.trackEcommerceItem = function (transactionId, sku, name, category, price, quantity) {
-                $log.log('ITEM Tracked', transactionId, sku, name, category, price, quantity);
-            };
-            //BE added for ecommerce transaction push
-            service.pushTransaction = function () {
-                $log.log('TRANSACTION Pushed', 'Success!');
-            };
-
             return service;
         }
     ]);
 }());
 (function () {
-    angular.module('angul3rytics').factory('Angul3ryticsGoogleHandler', [
-        '$log',
-        function ($log) {
-            var service = {};
-            service.trackPageView = function (url) {
-                _gaq.push([
-                    '_set',
-                    'page',
-                    url
-                ]);
-                _gaq.push([
-                    '_trackPageview',
-                    url
-                ]);
-            };
-            service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
-                _gaq.push([
-                    '_trackEvent',
-                    category,
-                    action,
-                    opt_label,
-                    opt_value,
-                    opt_noninteraction
-                ]);
-            };
-
-            //BE added for ecommerce order tracking
-            //(transactionId, affiliation, total, tax, shipping, city, state, country)
-            service.trackEcommerceTrans = function (transactionId, affiliation, total, tax, shipping, city, state, country) {
-                _gaq.push([
-                    '_addTrans',
-                    transactionId,
-                    affiliation,
-                    total,
-                    tax,
-                    shipping,
-                    city,
-                    state,
-                    country
-                ]);
-            };
-            //BE added for ecommerce item tracking
-            //(transactionId, sku, name, category, price, quantity)
-            service.trackEcommerceItem = function (transactionId, sku, name, category, price, quantity) {
-                _gaq.push([
-                    '_addItem',
-                    transactionId,
-                    sku,
-                    name,
-                    category,
-                    price,
-                    quantity
-                ]);
-            };
-            //BE added for ecommerce transaction push
-            service.pushTransaction = function () {
-                _gaq.push(['_trackTrans']);
-            };
-
-            return service;
-        }
-    ]).factory('Angul3ryticsGoogleUniversalHandler', function () {
+    angular.module('angul3rytics').factory('Angul3ryticsGoogleHandler', function () {
+        var service = {};
+        service.trackPageView = function (url) {
+            _gaq.push([
+                '_set',
+                'page',
+                url
+            ]);
+            _gaq.push([
+                '_trackPageview',
+                url
+            ]);
+        };
+        service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
+            _gaq.push([
+                '_trackEvent',
+                category,
+                action,
+                opt_label,
+                opt_value,
+                opt_noninteraction
+            ]);
+        };
+        service.trackTiming = function (category, variable, value, opt_label) {
+            _gaq.push([
+                '_trackTiming',
+                category,
+                variable,
+                value,
+                opt_label
+            ]);
+        };
+        return service;
+    }).factory('Angul3ryticsGoogleUniversalHandler', function () {
         var service = {};
         service.trackPageView = function (url) {
             ga('set', 'page', url);
@@ -192,9 +124,9 @@
         service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
             ga('send', 'event', category, action, opt_label, opt_value, { 'nonInteraction': opt_noninteraction });
         };
-    service.trackTiming = function (category, variable, value, opt_label) {
-      ga('send', 'timing', category, variable, value, opt_label);
-    };
+        service.trackTiming = function (category, variable, value, opt_label) {
+            ga('send', 'timing', category, variable, value, opt_label);
+        };
         return service;
     });
 }());
@@ -204,36 +136,6 @@
         function (Angul3rytics) {
             return function (entry, category, action, opt_label, opt_value, opt_noninteraction) {
                 Angul3rytics.trackEvent(category, action, opt_label, opt_value, opt_noninteraction);
-                return entry;
-            };
-        }
-    ]);
-    //BE added for ecommerce order tracking
-    angular.module('angul3rytics').filter('trackEcommerceTrans', [
-        'Angul3rytics',
-        function (Angul3rytics) {
-            return function (entry, transactionId, affiliation, total, tax, shipping, city, state, country) {
-                Angul3rytics.trackEcommerceTrans(transactionId, affiliation, total, tax, shipping, city, state, country);
-                return entry;
-            };
-        }
-    ]);
-    //BE added for ecommerce item tracking
-    angular.module('angul3rytics').filter('trackEcommerceItem', [
-        'Angul3rytics',
-        function (Angul3rytics) {
-            return function (entry, transactionId, sku, name, category, price, quantity) {
-                Angul3rytics.trackEcommerceTrans(transactionId, sku, name, category, price, quantity);
-                return entry;
-            };
-        }
-    ]);
-    //BE added for ecommerce item tracking
-    angular.module('angul3rytics').filter('pushTransaction', [
-        'Angul3rytics',
-        function (Angul3rytics) {
-            return function (entry) {
-                Angul3rytics.pushTransaction();
                 return entry;
             };
         }
